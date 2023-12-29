bc5e1427490ba8b54e8a8e4b120eeb046b8df3d6
Anton Shiryaev <med.phisiker@gmail.com>
2023-12-30 03:43:18 +1000

    refactor(model_name): вывести название модели в отдельный параметр
    вывести название модели в отдельный параметр

--- src/models/get_embeding_from_lm.py
+++ src/models/get_embeding_from_lm.py
@@ -1,7 +1,8 @@
 from sentence_transformers import SentenceTransformer
 
 if __name__ == "__main__":
-    model = SentenceTransformer("cointegrated/rubert-tiny2")
+    model_name = "cointegrated/LaBSE-en-ru"
+    model = SentenceTransformer(model_name)
     sentences = ["привет мир", "hello world", "здравствуй вселенная"]
     embeddings = model.encode(sentences)
     for embedding in embeddings:
